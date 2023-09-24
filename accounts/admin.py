8fef2420e42ae7a30cf40b6859a7b4985337ba95
evgx81 <xoroshix2002@mail.ru>
2023-09-24 11:04:43 +0300

    Добавлена модель пользователя и создана база данных

---
+++ accounts/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import MLSUserChangeForm, MLSUserCreationForm
+from .models import MLSUser
+
+MLSUser = get_user_model()
+
+
+class MLSUserAdmin(UserAdmin):
+    add_form = MLSUserCreationForm
+    form = MLSUserChangeForm
+    model = MLSUser
+    list_display = (
+        "email",
+        "username",
+    )
+
+
+admin.site.register(MLSUser, MLSUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class MLSUserCreationForm(UserCreationForm):
+    
+    class Meta:
+        model = get_user_model()
+        fields = ("email", "username",)
+
+    
+class MLSUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ("email", 'username')
+
